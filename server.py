6cec86fed44ed24acc9e269fed2c4d6402ec38b5
Trevor Anderson <tbone101@gmail.com>
2021-10-12 21:29:04 -0700

    Fiished Assignment

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+import datetime
 app = Flask(__name__)  
 
 @app.route('/')         
@@ -8,7 +9,15 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    val1 = int(request.form['strawberry'])
+    val2 = int(request.form['raspberry'])
+    val3 = int(request.form['apple'])
+    firstName = request.form['first_name']
+    lastName = request.form['last_name']
+    studentId = request.form['student_id']
+    valTotal = val1 + val2 + val3
+    return render_template("checkout.html", val1=val1, val2=val2, val3= val3, valTotal=valTotal, firstName=firstName, lastName=lastName, studentId=studentId, date=date)
 
 @app.route('/fruits')         
 def fruits():
